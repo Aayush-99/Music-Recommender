ce75b4d3ecd89a4d6b240036009698818d19e010
Aayush Agarwal <aayush.88@outlook.com>
2020-04-28 12:44:07 +0530

    Delete connect.inc.php

--- Website/connect.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = @mysqli_connect('localhost','root','','a_database');
-
-if (mysqli_connect_errno()) {
-	echo 'Failed to connect to MySQL Database : '.mysqli_connect_error();
-	exit();
-}
-?>
